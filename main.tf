9a30414edeea6e269e9bfe6a9ad95f4f873f6e09
Mana2704 <138353777+Mana2704@users.noreply.github.com>
2024-02-16 10:01:29 +0000

    cha

---
+++ main.tf
@@ -0,0 +1,23 @@
+resource "google_compute_instance" "vm-for-terraform-1" {
+  name         = "vm-pavan"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value" 
+      }
+    }
+  }
+  network_interface {
+    network = "default"
+  }
+
+ metadata_startup_script = "echo hi > /test.txt"
+ # allow_stopping_for_update = true
+}
+
+
+
