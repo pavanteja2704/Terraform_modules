96ceb268003bee7a2d48dc34b7ff6171e28be00d
Mana2704 <138353777+Mana2704@users.noreply.github.com>
2024-02-17 06:40:17 +0000

    r

--- main.tf
+++ main.tf
@@ -1,22 +1,20 @@
-resource "google_compute_instance" "vm-tf" {
-  name         = "pavan-cicd"
-  machine_type = "e2-medium"
+resource "google_compute_instance" "default" {
+  name         = "my-CICD"
+  machine_type = "n2-standard-2"
   zone         = "us-central1-a"
 
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
       labels = {
-        my_label = "value" 
+        my_label = "value"
       }
     }
   }
+
   network_interface {
     network = "default"
   }
-
- metadata_startup_script = "echo hi > /test.txt"
- # allow_stopping_for_update = true
 }
 
 
