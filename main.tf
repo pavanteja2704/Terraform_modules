58fdca21d3c5d27f8638681016562f73cec5e8d6
Mana2704 <138353777+Mana2704@users.noreply.github.com>
2024-02-20 10:44:01 +0000

    bd

--- main.tf
+++ main.tf
@@ -10,8 +10,23 @@ resource "google_compute_instance" "default" {
   metadata = {
     startup-script = <<-EOF
     #!/bin/bash
+
+    # Install Apache2
     sudo apt-get update
-    sudo apt-get install -y python3
+    sudo apt-get install apache2 -y
+
+    # Start Apache2
+    sudo systemctl start apache2
+
+    # Check if Apache2 is running
+    sudo systemctl status apache2 | grep "active (running)"
+
+    # Open port 80 on the firewall
+    sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+
+    # Save the firewall rules
+    sudo iptables-save
+
     EOF
   }
  network_interface {
